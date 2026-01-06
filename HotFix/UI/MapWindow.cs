d7750cb0e586b24eb710a071dc6506cb622764b0
qw-12hdfd <15176699658@163.com>
2026-01-06 14:57:51 +0800

    123

---
+++ HotFix/UI/MapWindow.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace HotFix
+{
+  public  class MapWindow: Window
+    {
+        private   Button siYangChangBtn;
+        private Button fanTuChangBtn;
+        private Button saiMaChangBtn;
+        private Button closeBtn;
+        RawImage rawImage;
+        Camera camera;
+        public override void Awake(object param1 = null, object param2 = null, object param3 = null) { 
+            GetALLComponent();
+            AddALLBtnistener();
+        }
+        void AddALLBtnistener() { 
+            AddButtonClickListener(closeBtn, CloseWndFunc);
+            AddButtonClickListener(siYangChangBtn, GoToSiYangChang);
+            AddButtonClickListener(fanTuChangBtn, GoToSaiMachang);
+            AddButtonClickListener(saiMaChangBtn, GoToFanYunchang);
+
+        }
+        void CloseWndFunc()
+        {
+            UIManager.instance.CloseWnd(this);
+        }
+        void GoToSiYangChang() {
+            MessageCenter.instance.Dispatch(MessageCenterEventID.PlayerChangePosition,new Notification(3));
+        }
+        void GoToSaiMachang()
+        {
+            MessageCenter.instance.Dispatch(MessageCenterEventID.PlayerChangePosition, new Notification(2));
+        }
+        void GoToFanYunchang()
+        {
+            MessageCenter.instance.Dispatch(MessageCenterEventID.PlayerChangePosition, new Notification(1));
+        }
+        void GetALLComponent() {
+            siYangChangBtn=m_Transform.Find("MapBack/siyangchang").GetComponent<Button>();
+            fanTuChangBtn = m_Transform.Find("MapBack/fanzhichang").GetComponent<Button>();
+            saiMaChangBtn = m_Transform.Find("MapBack/saimachang").GetComponent<Button>();
+            closeBtn=m_Transform.Find("MapBack/Close").GetComponent<Button>();
+            rawImage = m_Transform.Find("MapBack/RawImage").GetComponent<RawImage>();
+            camera = m_Transform.Find("Camera").GetComponent<Camera>();
+        }
+    }
+}
