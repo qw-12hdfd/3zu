cf81352e213d6521c04a149f52596cbfc672a856
qw-12hdfd <15176699658@163.com>
2026-01-07 15:59:18 +0800

    Revert "123最新"
    This reverts commit 0f8a67b9291983e285de17feb350ddcec85eb2f6.
    
    # Conflicts:
    #	HotFix/Objects/PlayerController.cs   resolved by [回滚] version
    #	HotFix/UI/MapWindow.cs   resolved by [回滚] version
    #	HotFix/obj/Debug/HotFix.pdb   resolved by [回滚] version

--- HotFix/Objects/PlayerController.cs
+++ HotFix/Objects/PlayerController.cs
@@ -113,7 +113,7 @@ namespace HotFix
         public override void Awake(object param1 = null, object param2 = null, object param3 = null)
         {
             name = m_Transform.Find("Name").GetComponent<TextMesh>();
-            name.text = "QWE";
+            name.text = "huangmao";
             name.gameObject.SetActive(true);
             name.color = Color.green;
             moveAction = Move;
@@ -131,31 +131,34 @@ namespace HotFix
             triggerEvent.TriggerEnter = SetTriggerEnter;
             triggerEvent.TriggerExit = SetHorseExit;
             UserInfoManager.camera.GetComponent<Animator>().enabled = false;
-            AddALLEventListener();
+            AddAllEventListener();
         }
-        void AddALLEventListener()
+        void AddAllEventListener()
         {
             MessageCenter.instance.AddListener(MessageCenterEventID.PlayerChangePosition, PlayerChangePosition);
         }
+
         void PlayerChangePosition(Notification notification)
         {
             int type = (int)notification.content[0];
             Transform target = null;
             UserInfoManager.cmCamera.gameObject.SetActive(false);
             playerCtl.enabled = false;
+
             switch (type)
             {
-                case 1://饲养场
+                case 1:
                     target = UserInfoManager.transferPoint.Find("siyangchang");
+
                     m_Transform.position = target.position;
                     m_Transform.rotation = target.rotation;
                     break;
-                case 2://赛马场
+                case 2:
                     target = UserInfoManager.transferPoint.Find("saimachang");
                     m_Transform.position = target.position;
                     m_Transform.rotation = target.rotation;
                     break;
-                case 3://饲养场
+                case 3:
                     target = UserInfoManager.transferPoint.Find("fanzhichang");
                     m_Transform.position = target.position;
                     m_Transform.rotation = target.rotation;
@@ -167,11 +170,12 @@ namespace HotFix
             playerCtl.enabled = true;
             UIManager.instance.CloseWnd(FilesName.MAPPANEL);
         }
+
         private void PlayerGoToPos(string name)
         {
             UserInfoManager.cmCamera.gameObject.SetActive(false);
             Transform playerObj = null;
-            if (horse!=null)
+            if (horse != null)
                 playerObj = horse.transform;
             else
                 playerObj = m_Transform;
@@ -256,7 +260,7 @@ namespace HotFix
                     break;
             }
         }
-        
+
 
         private void SetTriggerEnter2(Collider collider)
         {
@@ -278,7 +282,7 @@ namespace HotFix
 
         public void GetQuestionFunc()
         {
-            if (mount&&UIManager.instance.IsSignWindowOpen(FilesName.MAINPANEL))
+            if (mount && UIManager.instance.IsSignWindowOpen(FilesName.MAINPANEL))
             {
                 JsonData data = new JsonData();
                 data["horseId"] = UserInfoManager.mountHorseID;
@@ -352,7 +356,7 @@ namespace HotFix
             MainWindow.SetBtnGray(false);
             UserInfoManager.mount = true;
             m_Transform.SetParent(horse.transform.Find("CG/Pelvis/Spine/PlayerPos")); //人物跟随马匹身上的父节点
-            if(UserInfoManager.Sex == 1)
+            if (UserInfoManager.Sex == 1)
             {
                 horse.transform.Find("CG/Pelvis/Spine/PlayerPos").localPosition = new Vector3(-0.5053826f, 1.195526f, 0.5479326f);
             }
@@ -363,7 +367,7 @@ namespace HotFix
             playerCtl.enabled = false; //将人物控制器关闭 否则无法移动位置
             m_Transform.ResetLocal();//数据归零
             animator.SetTrigger("MountHorse"); //播放上马动画
-            MainWindow.MountHorse(false,false);//关闭上马按钮
+            MainWindow.MountHorse(false, false);//关闭上马按钮
             MainWindow.PutFood(false, id, horseid);
             horseAni = horse.GetComponent<Animator>();
             horseTrigger = horse.GetComponent<TriggerEvent>();
@@ -394,15 +398,15 @@ namespace HotFix
             switch (collider.gameObject.tag)
             {
                 case "MaCao":
-                    MainWindow.MountHorse(false,false );
-                    MainWindow.PutFood(false, 0,0);
-                    if(UserInfoManager.maCaoTransform&& UserInfoManager.maCaoTransform.Find("HorsePos").childCount>0)
+                    MainWindow.MountHorse(false, false);
+                    MainWindow.PutFood(false, 0, 0);
+                    if (UserInfoManager.maCaoTransform && UserInfoManager.maCaoTransform.Find("HorsePos").childCount > 0)
                     {
                         Animator ani = UserInfoManager.maCaoTransform.Find("HorsePos").GetChild(0).GetComponent<Animator>();
                         if (!ani.GetBool("Eat") && UserInfoManager.maCaoTransform.Find("fodders").GetChild(0).gameObject.activeSelf && !mount)
                             ani.SetBool("Eat", true);
                     }
-                    
+
                     UserInfoManager.maCaoTransform = null;
                     break;
                 case "Door":
@@ -441,7 +445,7 @@ namespace HotFix
 
         public override void OnUpdate()
         {
-            if (mount&&horseAni!=null&&horseAni.GetBool("Eat"))
+            if (mount && horseAni != null && horseAni.GetBool("Eat"))
             {
                 horseAni.SetBool("Eat", false);
             }
@@ -478,7 +482,7 @@ namespace HotFix
                             }
                             break;
                         case "CreateRoom":
-                            WebRequestManager.instance.AsyncLoadUnityWebRequest(WebRequestUtils.listFrontUrl, WebRequestFuncitons.GetMyMHorsesList, true, JsonMapper.ToJson(new HorseSex(0, 2,1,1)), RFrameWork.instance.token);
+                            WebRequestManager.instance.AsyncLoadUnityWebRequest(WebRequestUtils.listFrontUrl, WebRequestFuncitons.GetMyMHorsesList, true, JsonMapper.ToJson(new HorseSex(0, 2, 1, 1)), RFrameWork.instance.token);
                             break;
                         case "FZCDoor":
                             if (hit.transform.parent.Find("Room_tips").Find("red").gameObject.active)
@@ -529,7 +533,7 @@ namespace HotFix
                         animator.SetBool(walkState, false);
                         animator.SetInteger("Jump", 2);
                     }
-                    if(animator.GetBool(walkState))
+                    if (animator.GetBool(walkState))
                         playerCtl.Move(moveDirection * Time.deltaTime * speed);
                 }
             }
@@ -580,16 +584,16 @@ namespace HotFix
                 }
                 if (isGround && mount)
                 {
-                    if(horseAni.GetBool("Fall"))
-                    horseAni.SetBool("Fall", false);
-                    if(!moving)
-                    getDownHorse = true;
+                    if (horseAni.GetBool("Fall"))
+                        horseAni.SetBool("Fall", false);
+                    if (!moving)
+                        getDownHorse = true;
                 }
             }
 
             if (UnityEngine.Input.GetKey(KeyCode.Space))
             {
-                if(jumping == false && isGround)
+                if (jumping == false && isGround)
                 {
                     Jump();
                 }
@@ -616,7 +620,7 @@ namespace HotFix
                 var colliders = Physics.OverlapCapsule(pointBottom, pointTop, 0.2f, ignoreMask);
                 var colliders2 = Physics.OverlapCapsule(pointBottom, pointTop, 0.2f, ignoreMask2);
                 Debug.DrawLine(pointBottom, pointTop, Color.yellow);
-                if (colliders2.Length!=0)
+                if (colliders2.Length != 0)
                 {
                     isGround = true;
                     playerCtl.enabled = true;
--- HotFix/UI/MapWindow.cs
+++ HotFix/UI/MapWindow.cs
@@ -8,77 +8,48 @@ using UnityEngine.UI;
 
 namespace HotFix
 {
-    internal class MapWindow:Window
+  public  class MapWindow: Window
     {
-        private Button siyangchang;
-        private Button fanzhichang;
-        private Button saimachang;
-        private Button close;
-        private Camera camera;
-        private RawImage rImage;
-        public override void Awake(object param1 = null, object param2 = null, object param3 = null)
+        private   Button siYangChangBtn;
+        private Button fanTuChangBtn;
+        private Button saiMaChangBtn;
+        private Button closeBtn;
+        RawImage rawImage;
+        Camera camera;
+        RenderTexture showTest=new RenderTexture(512, 512, 16,RenderTextureFormat.ARGB32);
+        public override void Awake(object param1 = null, object param2 = null, object param3 = null) { 
+            GetALLComponent();
+            AddALLBtnistener();
+        }
+        void AddALLBtnistener() { 
+            AddButtonClickListener(closeBtn, CloseWndFunc);
+            AddButtonClickListener(siYangChangBtn, GoToSiYangChang);
+            AddButtonClickListener(fanTuChangBtn, GoToSaiMachang);
+            AddButtonClickListener(saiMaChangBtn, GoToFanYunchang);
+
+        }
+        void CloseWndFunc()
         {
-            siyangchang = m_Transform.Find("MapBack/siyangchang").GetComponent<Button>();
-            fanzhichang = m_Transform.Find("MapBack/fanzhichang").GetComponent<Button>();
-            saimachang = m_Transform.Find("MapBack/saimachang").GetComponent<Button>();
-            close = m_Transform.Find("MapBack/Close").GetComponent<Button>();
+            UIManager.instance.CloseWnd(this);
+        }
+        void GoToSiYangChang() {
+            MessageCenter.instance.Dispatch(MessageCenterEventID.PlayerChangePosition,new Notification(3));
+        }
+        void GoToSaiMachang()
+        {
+            MessageCenter.instance.Dispatch(MessageCenterEventID.PlayerChangePosition, new Notification(2));
+        }
+        void GoToFanYunchang()
+        {
+            MessageCenter.instance.Dispatch(MessageCenterEventID.PlayerChangePosition, new Notification(1));
+        }
+        void GetALLComponent() {
+            siYangChangBtn=m_Transform.Find("MapBack/siyangchang").GetComponent<Button>();
+            fanTuChangBtn = m_Transform.Find("MapBack/fanzhichang").GetComponent<Button>();
+            saiMaChangBtn = m_Transform.Find("MapBack/saimachang").GetComponent<Button>();
+            closeBtn=m_Transform.Find("MapBack/Close").GetComponent<Button>();
+            rawImage = m_Transform.Find("MapBack/RawImage").GetComponent<RawImage>();
             camera = m_Transform.Find("Camera").GetComponent<Camera>();
-            rImage = m_Transform.Find("MapBack/RawImage").GetComponent<RawImage>();
-            RenderTexture showTest = new RenderTexture(512, 512, 24, RenderTextureFormat.ARGB32);
-            camera.targetTexture = showTest;
-            rImage.texture = showTest;
-            AddButtonClickListener(siyangchang, GoToSiYangChang);
-            AddButtonClickListener(fanzhichang, GoToFanZhiChang);
-            AddButtonClickListener(saimachang, GoToSaiMaChang);
-            AddButtonClickListener(close, () => { UIManager.instance.CloseWnd(this); });
-        }
-
-        private void GoToSaiMaChang()
-        {
-            RFrameWork.instance.OpenCommonConfirm("提示", "您是否要传送到赛马场？", () => { PlayerController.GoToPosition("saimachang"); UIManager.instance.CloseWnd(this);
-                MainWindow.GetDownHorse(false);//关闭上马按钮
-                if ( UserInfoManager.maCaoTransform != null)
-                {
-                    MainWindow.MountHorse(false, false);
-                    MainWindow.PutFood(false, 0, UserInfoManager.mountHorseID);
-                    var ani = UserInfoManager.maCaoTransform.Find("HorsePos").GetChild(0).GetComponent<Animator>();
-                    if (!ani.GetBool("Eat") && UserInfoManager.maCaoTransform.Find("fodders").GetChild(0).gameObject.active && !UserInfoManager.playerCtrl.mount)
-                        ani.SetBool("Eat", true);
-                    UserInfoManager.maCaoTransform = null;
-                }
-            }, () => { });
-        }
-
-        private void GoToFanZhiChang()
-        {
-            RFrameWork.instance.OpenCommonConfirm("提示", "您是否要传送到繁育场？", () => { PlayerController.GoToPosition("fanzhichang"); UIManager.instance.CloseWnd(this);
-                MainWindow.GetDownHorse(false);//关闭上马按钮
-                if (UserInfoManager.maCaoTransform != null)
-                {
-                    MainWindow.MountHorse(false, false);
-                    MainWindow.PutFood(false, 0, UserInfoManager.mountHorseID);
-                    var ani = UserInfoManager.maCaoTransform.Find("HorsePos").GetChild(0).GetComponent<Animator>();
-                    if (!ani.GetBool("Eat") && UserInfoManager.maCaoTransform.Find("fodders").GetChild(0).gameObject.active && !UserInfoManager.playerCtrl.mount)
-                        ani.SetBool("Eat", true);
-                    UserInfoManager.maCaoTransform = null;
-                }
-            }, () => { });
-        }
-
-        private void GoToSiYangChang()
-        {
-            RFrameWork.instance.OpenCommonConfirm("提示", "您是否要传送到饲养场？", () => { PlayerController.GoToPosition("siyangchang"); UIManager.instance.CloseWnd(this);
-                MainWindow.GetDownHorse(false);//关闭上马按钮
-                if (UserInfoManager.maCaoTransform != null)
-                {
-                    MainWindow.MountHorse(false, false);
-                    MainWindow.PutFood(false, 0, UserInfoManager.mountHorseID);
-                    var ani = UserInfoManager.maCaoTransform.Find("HorsePos").GetChild(0).GetComponent<Animator>();
-                    if (!ani.GetBool("Eat") && UserInfoManager.maCaoTransform.Find("fodders").GetChild(0).gameObject.active && !UserInfoManager.playerCtrl.mount)
-                        ani.SetBool("Eat", true);
-                    UserInfoManager.maCaoTransform = null;
-                }
-            }, ()=> { });
         }
     }
 }
